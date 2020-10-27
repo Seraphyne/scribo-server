42aa3b26b6cd85975e8ed43eb0ca6539abbf0ece
seraphyne <renatafd717@gmail.com>
2020-10-26 20:55:55 -0500

    fixing connection to db

--- src/config.js
+++ src/config.js
@@ -1,5 +1,11 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://thingful@localhost/thingful',
-}
+  DATABASE_URL: process.env.DATABASE_URL ||
+    'postgresql://dunder_mifflin:2@localhost/thingful',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL ||
+    'postgresql://dunder_mifflin:2@localhost/thingful-test',
+  API_TOKEN: process.env.API_TOKEN,
+  JWT_SECRET: process.env.JWT_SECRET || 'scribo-super-secret',
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '2h',
+};
